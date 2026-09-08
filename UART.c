02d0c018222e2cac5c142cfa7bd2be0a3557dcc7
JITH1 <Jithinpk2002@gmail.com>
2026-09-09 01:05:46 +0530

    feat: Added UART Driver and interface

--- UART.c
+++ UART.c
@@ -1,10 +1,79 @@
 #include<xc.h>
 #include "UART.h"
 
-void init_UART()
+void init_uart(void)
 {
     
+	RX_PIN = 1;
+	TX_PIN = 0;
+
+	BAUD_RATE_16_BIT = ENABLE_16_BIT_BAUD ;
+    BAUD_RATE_SELECT_BIT = HIGH_SPEED ;
+    BAUD_RATE_HIGH = HIGH_VALUE ;
+    BAUD_RATE_LOW = LOW_VALUE ;
+    
+    EUSART_MODE = ASYNCHRONOUS ;
+    BIT_TRANSMISSION = EIGHT_BIT;
+    TRANSMIT_ENABLE = ENABLE_TRANSMIT;
+    SEND_BREAK_CHAR_BIT = BREAK_TXN_COMPLETED;
+    
+    RECIVE_BIT_ENABLE = EIGHT_BIT_RECIEVE;
+    CONTINUOUS_RECIVE = ENABLE_CONT_RECIEVE;
+    SERIAL_PORT = SERIAL_PORT_ENABLED ;
+    
+    AUTO_BAUD_DETECT = DISABLE ;
+	/* TX interrupt flag bit */
+	Tx_INT_FLAG = 0;
+
+	/* RX interrupt enable bit */
+	Rx_INT_FLAG = 0;
+    
 }
+
+void put_ch(unsigned char byte) 
+{
+	/* Output one byte */
+	/* Set when register is empty */
+	while(!Tx_INT_FLAG)
+	{
+		continue;
+	}
+	Tx_INT_FLAG = 0;
+	TXREG = byte;
+} 
+
+int puts(const char *s)
+{
+	while(*s)		
+	{
+		put_ch(*s++);	
+	}
+	return 0;
+}
+
+unsigned char get_ch(void)
+{
+	/* Retrieve one byte */
+	/* Set when register is not empty */
+	while(!RCIF)
+	{
+		continue;
+	}
+	RCIF = 0;
+	return RCREG;
+}
+
+unsigned char getche(void)
+{
+	unsigned char c;
+
+	put_ch(c = get_ch());
+
+	return (c);
+}
+
+
+
 
 
 
--- UART.h
+++ UART.h
@@ -1,6 +1,45 @@
 #ifndef UART_H
 #define UART_H
 
-void init_UART(void);
+#define RX_PIN					      TRISC7
+#define TX_PIN					      TRISC6
+
+#define BAUD_RATE_16_BIT              BAUDCONbits.BRG16
+#define ENABLE_16_BIT_BAUD            1
+
+#define BAUD_RATE_SELECT_BIT          TXSTAbits.BRGH
+#define HIGH_SPEED                    1
+
+#define BAUD_RATE_HIGH                SPBRGH
+#define BAUD_RATE_LOW                 SPBRG
+#define HIGH_VALUE                    0x02
+#define LOW_VALUE                     0x08
+#define AUTO_BAUD_DETECT              BAUDCONbits.ABDEN
+
+#define EUSART_MODE                   TXSTAbits.SYNC
+#define ASYNCHRONOUS                  0
+#define BIT_TRANSMISSION              TXSTAbits.TX9
+#define EIGHT_BIT                     0
+#define TRANSMIT_ENABLE               TXSTAbits.TXEN
+#define ENABLE_TRANSMIT               1
+#define SEND_BREAK_CHAR_BIT           TXSTAbits.SENDB
+#define BREAK_TXN_COMPLETED           0
+
+#define RECIVE_BIT_ENABLE             RCSTAbits.RX9
+#define EIGHT_BIT_RECIEVE             0
+#define CONTINUOUS_RECIVE             RCSTAbits.CREN
+#define ENABLE_CONT_RECIEVE           1
+#define DISABLE                       0
+#define SERIAL_PORT                   RCSTAbits.SPEN
+#define SERIAL_PORT_ENABLED            1
+
+#define Tx_INT_FLAG                   PIR1bits.TXIF
+#define Rx_INT_FLAG                   PIR1bits.RCIF
+
+void init_uart(void);
+void put_ch(unsigned char byte);
+int puts(const char *s);
+unsigned char get_ch(void);
+unsigned char getche(void);
 
 #endif
